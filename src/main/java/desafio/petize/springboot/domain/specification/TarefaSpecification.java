9c2f262d92cc721c8015de299268632644a3cc3d
Rafael <ribeiro.rafajunior@gmail.com>
2025-08-12 15:22:30 -0300

    feat : Adicionando Specification para filtragem

--- src/main/java/desafio/petize/springboot/domain/specification/TarefaSpecification.java
+++ src/main/java/desafio/petize/springboot/domain/specification/TarefaSpecification.java
@@ -1,4 +1,27 @@
 package desafio.petize.springboot.domain.specification;
 
+import desafio.petize.springboot.domain.tarefa.Prioridade;
+import desafio.petize.springboot.domain.tarefa.Status;
+import desafio.petize.springboot.domain.tarefa.Tarefa;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+
 public class TarefaSpecification {
+
+    public static Specification<Tarefa> naoDeletado() {
+        return (root, query, cb) -> cb.notEqual(root.get("status"), Status.DELETADO);
+    }
+
+    public static Specification<Tarefa> comStatus(Status status) {
+        return (root, query, criteriaBuilder) -> status == null ? criteriaBuilder.conjunction() : criteriaBuilder.equal(root.get("status"), status);
+    }
+
+    public static Specification<Tarefa> comPrioridade(Prioridade prioridade) {
+        return (root, query, criteriaBuilder) -> prioridade == null ? criteriaBuilder.conjunction() : criteriaBuilder.equal(root.get("prioridade"), prioridade);
+    }
+
+    public static Specification<Tarefa> comDataVencimento(LocalDate dataVencimento) {
+        return (root, query, criteriaBuilder) -> dataVencimento == null ? criteriaBuilder.conjunction() : criteriaBuilder.equal(root.get("dataVencimento"), dataVencimento);
+    }
 }
